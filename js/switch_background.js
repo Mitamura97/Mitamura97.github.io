5f33d1b89126abc94747be20f928a358a49ef4a2
Mitamura97 <didnot00@gmail.com>
2017-02-22 20:37:46 +0900

    1.3.1

--- js/switch_background.js
+++ js/switch_background.js
@@ -1,8 +1,10 @@
-/*画面の高さを取得*/
+$(function(){
+  /*画面の高さを取得*/
 var h = document.documentElement.clientHeight;
 /*スクロール量の取得*/
 $(window).scroll(function(){ // スクロール毎にイベントが発火
   var y = jquery(this).scrollTop();
-  var c = Math.floor(y/h);
+document.write(y);
+});
 
-})
+});
